79c06184e9a564a58ca9359d99e0ade0d3617749
iliadavitashvili <81050721+iliadavitashvili@users.noreply.github.com>
2024-02-15 15:17:38 +0400

    about section started

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ const swiper = new Swiper('.swiper', {
      autoplay: {
      delay: 5000,
    },
-  
-   allowTouchMove:false
- 
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+   allowTouchMove:false,
+   pagination: {
+    el: '.swiper-pagination',
+  },
+   autoplay: {
+   delay: 5000,
+ },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   });
