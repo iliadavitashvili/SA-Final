bbef89c70a2ad3eacdf9796ec373092c6d01eea0
iliadavitashvili <81050721+iliadavitashvili@users.noreply.github.com>
2024-02-16 02:20:46 +0400

    about section finished

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
 
-const swiper = new Swiper('.swiper', {
-
-    direction: 'horizontal',
-  
-  
-  
-     autoplay: {
-     delay: 5000,
-   },
+  autoplay: {
+    delay: 5000,
+  },
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
   },
-   allowTouchMove:false,
-   pagination: {
-    el: '.swiper-pagination',
+  allowTouchMove: false,
+  pagination: {
+    el: ".swiper-pagination",
   },
-   autoplay: {
-   delay: 5000,
- },
+  autoplay: {
+    delay: 5000,
+  },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-  });
+});
